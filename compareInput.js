899c9015729c0e88c011c595e07c1702c0891824
Edgar <aguirre.edgar567@gmail.com>
2020-07-14 01:34:00 -0500

    added compareInput.js

---
+++ compareInput.js
@@ -0,0 +1,17 @@
+// Setup
+function compareEquality(inputOne, InputTwo) {
+    if (inputOne === inputTwo) { // Change this line
+      return "Equal";
+    } 
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
+  
+//   Comparision Operators 
+//   !=    not equal to 
+//   !==   strickly not equal to / not exactly the same
+
+// https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comparisons-with-the-logical-or-operator
+
+//Tests 
