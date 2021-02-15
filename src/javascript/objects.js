d49df580ecf9a9bcf4865c46a7dfac2d42fcecac
Edgar <aguirre.edgar567@gmail.com>
2021-02-14 19:02:06 -0600

    interate nested objects

--- src/javascript/objects.js
+++ src/javascript/objects.js
@@ -1,15 +1,54 @@
-// Setup
-var testObj = {
-    12: "Namath",
-    16: "Montana",
-    19: "Unitas"
-  };
+// // Setup
+// var testObj = {
+//     12: "Namath",
+//     16: "Montana",
+//     19: "Unitas"
+//   };
   
-  // Only change code below this line
+//   // Only change code below this line
   
-  var playerNumber = 16 ;       // 
-  console.log(playerNumber);
+//   var playerNumber = 16 ;       // 
+//   console.log(playerNumber);
 
-  var player = testObj[playerNumber];   // 
-   console.log(player);
-   
+//   var player = testObj[playerNumber];   // 
+//    console.log(player);
+
+var contacts = [
+  {
+      "firstName": "Akira",
+      "lastName": "Laine",
+      "number": "0543236543",
+      "likes": ["Pizza", "Coding", "Brownie Points"]
+  },
+  {
+      "firstName": "Harry",
+      "lastName": "Potter",
+      "number": "0994372684",
+      "likes": ["Hogwarts", "Magic", "Hagrid"]
+  },
+  {
+      "firstName": "Sherlock",
+      "lastName": "Holmes",
+      "number": "0487345643",
+      "likes": ["Intriguing Cases", "Violin"]
+  },
+  {
+      "firstName": "Kristian",
+      "lastName": "Vos",
+      "number": "unknown",
+      "likes": ["JavaScript", "Gaming", "Foxes"]
+  }
+];
+function lookUpProfile(name, prop) {
+  //interact through contacts if you find the contact with matching name continue to match prop, else no property , else no match
+  for (let i = 0; i < contacts.length; i++) {
+    if (contacts[i].firstName === name) {
+      if (prop in contacts[i]) {
+        return contacts[i][prop];
+      } else {
+        return "No such property";
+      }
+    }
+  }
+  return "No such contact";
+}
